63e30b712d01d3c6b45ff4d3bc0ead3a93b6b592
Poonam Kumari <135123668+PoonamKumari44@users.noreply.github.com>
2023-06-14 00:15:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,17 +16,10 @@ let clocksite=()=>{
     dy=d.getDay()
     mn=d.getMonth()
     yr=d.getFullYear()
-    // document.write(`CPU:${cpu} <br> User:${user}<br>The Winner is: ${result}`)
+ 
     document.getElementById("day").innerHTML = `${da} : ${mn} : ${yr} , ${vardays[dy]}`
-    // document.getElementById("month").innerHTML = mn
-    // document.getElementById("yr").innerHTML = yr
     document.getElementById("hour").innerHTML = `${h} : ${m} : ${s}`
-    // document.getElementById("semi1").innerHTML =":"
-    // document.getElementById("minute").innerHTML = m
-    // document.getElementById("semi2").innerHTML =":"
-    // document.getElementById("second").innerHTML = s
-    // document.write(`<br>`)
-    
+ 
   }, 850);
 }
 clocksite();
